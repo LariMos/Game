ac36acd2b56e50487223c5aa5044d317746538ff
larimos <larisa.miriana@gmail.com>
2023-05-11 21:36:35 -0400

    Progress

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //defining the board
 
 //Define main players - weasel and rabbit
@@ -8,69 +9,178 @@
 
 //Button to toggle between intro page and game page
 const clickToPlay = document.getElementById("click-to-play");
-const introPage = document.getElementById("intro-page");
-const gamePage = document.getElementById("game-page");
+const backButton = document.getElementById("return-to-intro");
 
-clickToPlay.addEventListener("click", function(){
-    if (introPage.style.display === "none"){
+const introPage = document.getElementById("intro-page");
+const instructionsPage = document.getElementById("instructions-page")
+const gamePage = document.getElementById("game-page");
+const speechAnimationBubble = document.getElementById("lottie-speech-bubble");
+
+clickToPlay.addEventListener("click", function () {
+    if (introPage.style.display === "none") {
         introPage.style.display = "block";
+        instructionsPage.style.display = "none";
         gamePage.style.display = "none";
-    }else{
+    } else {
         introPage.style.display = "none";
-        gamePage.style.display = "block";
+        instructionsPage.style.display = "block";
+        gamePage.style.display = "none";
     }
 });
 
-//Game intro speech bubbles --TO FIX 
+// backButton.addEventListener("click", function () {
+//     if (introPage.style.display === "none") {
+//         introPage.style.display = "block";
+//         instructionsPage.style.display = "none";
+//         gamePage.style.display = "none";
+//     } else {
+//         introPage.style.display = "none";
+//         instructionsPage.style.display = "block";
+//         gamePage.style.display = "none";
+//     }
+// });
 
-const speechBubble = document.querySelector(".speech-bubble");
 
-setTimeout(() => {
-  speechBubble.style.display = "none";
-}, 5000);
+class SpeechBubble {
+    constructor(bubbleId, message) {
+        this.bubbleId = document.getElementById(bubbleId);
+        this.message = message;
+    }
+    disappear(time) {
+        this.bubbleId.style.display = "block";
+        setTimeout(() => {
+            if (instructionsPage.style.display === "block"){
+            this.bubbleId.style.display = "none";
+            }            
+            speechAnimationBubble.style.display = "none";            
+            if (gamePage.style.display === "none") {
+                gamePage.style.display = "block";
+            }
+        }, time);
+        setTimeout(() => {
+            document.querySelector('#weasel-hello').style.display = "none";
+        }, time)
+    }
+
+    addMessage() {
+        this.bubbleId.innerHTML = this.message;
+    }
+}
+
+let helloWeasel = new SpeechBubble("hello", "Hi, I'm mOs the weasel and I've got my eye on an extra furry rabbit! Can you help me catch it? Click on the colors and let's bring it closer!");
+helloWeasel.addMessage();
+helloWeasel.disappear(5000);
+// console.log(helloWeasel);
 
 
 
 
-//Create color buttons
 
-let colorButtons = document.getElementsByClassName("color-button-container");
 
+const colorButtons = document.getElementsByClassName("color-buttons");
+const weaselImages = document.getElementsByClassName("weasel-images");
+const playerScore = document.getElementById("player-score");
+let correctClicks = 0;
+let totalFlashes = 0;
+let count = 0;
+let interval;
+
+const readySetGoText = document.getElementById("ready-set-go");
+
+function countdown() {
+    if (gamePage.style.display === "block") {
+        setTimeout(function() {
+            readySetGoText.textContent = "Ready?!";
+            
+            setTimeout(function() {
+              readySetGoText.textContent = "Set";
+              
+              setTimeout(function() {
+                readySetGoText.textContent = "Go!";
+
+                setTimeout(function() {
+                    readySetGoText.textContent = "";
+                },1000);        
+
+              }, 1000);
+            }, 1000);
+          }, 1000);
+    }
+  }
 
 function flashButton(colorButton) {
-    colorButton.style.opacity = "0.1";
-    setTimeout(function() {
-      colorButton.style.opacity = "1";
-    }, Math.random() * 500 + 100);
-  }
+    colorButton.classList.add("flashing");
+    colorButton.style.opacity = "1";
+    setTimeout(function () {
+        colorButton.classList.remove("flashing");
+        colorButton.style.opacity = "0.1";
+    }, 1000);
+}
 
-  function flashButtons() {
-    let count = 0;
-    let currentIndex = 0;
-    let interval = setInterval(function() {
-      flashButton(colorButtons[currentIndex]);
-      currentIndex++;
-      if (currentIndex >= colorButtons.length) {
-        currentIndex = 0;
-        count++;
-      }
-      if (count >= 15) {
-        clearInterval(interval);
-      }
-    }, Math.random() * 700 + 500);
-  }
-  
-  flashButtons();  
+function flashImage(weaselImage) {
+    weaselImage.style.display = "block";
+    setTimeout(function () {
+        weaselImage.style.display = "none";
+    }, 1000);
+}
+
+function flashButtons() {
+    let totalFlashes = 0;
+    let delayBetweenEachButtonFlash = 800;
+    let interval = setInterval(() => {
+        let delay = 0;
+        for (let i = 0; i < colorButtons.length; i++) {
+            setTimeout(() => {
+                flashButton(colorButtons[i]);
+                flashImage(weaselImages[i]);
+            }, delay);
+            delay += delayBetweenEachButtonFlash;
+        }
+        totalFlashes++;
+        if (totalFlashes >= 15) {
+            clearInterval(interval);
+            let percentage = (correctClicks / (totalFlashes * colorButtons.length)) * 100;
+            if (percentage >= 50) {
+                alert("Yes!!! I'm close enough to catch it!");
+            } else {
+                alert("Darn it's too far! I won't catch it...Better luck next time!");
+            }
+        }
+    }, colorButtons.length * delayBetweenEachButtonFlash);
+}
+
+function handleClick(e) {
+    const target = e.target;
+    if (target.classList.contains("color-buttons") && target.classList.contains("flashing")) {
+        correctClicks++;
+        playerScore.innerText = correctClicks;
+    }
+}
+
+const rabbitPop1=document.getElementById("rabbit-pop-1");
+function rabbitPop(){
+    if (correctClicks >=3 && rabbitPop1.style.display === "none"){
+        rabbitPop1.style.display = "block"; 
+    }
+}
 
 const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", flashButtons);
+startButton.addEventListener("click", () => {
+    correctClicks = 0;
+    count = 0;
+    countdown();
+    playerScore.innerText = correctClicks;
+    flashButtons();
+    rabbitPop();
+});
+for (let i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].addEventListener("click", handleClick);
+}
 
 
 
 
-
-//test for future code versions
-
+//I WANT TO REWRITE THE CODE USING CLASSES AFTER PROJECT DEADLINE
 
 // class Player{
 //     constructor(location, size){
@@ -99,22 +209,10 @@ startButton.addEventListener("click", flashButtons);
 //     }
 // }
 
-
-
-
-
-
-
-
-
-
-
 // set possible locations MOVE OPTIONS
 
 //initialize game
 //game states 
-
-
 
 // class Game(){
 
@@ -126,22 +224,23 @@ startButton.addEventListener("click", flashButtons);
 // }
 
 
-const RhythmConstructor = {
-    nextStep: () => {
-        // call each step at a time interval
-    },
-    rhythm: () => {
-        redBall(1000);
-        greenBall(400);
-        blueBall(1200);
-        yellowBall(600);
-    },
-    rhythm2: () => {
-        redBall(1000);
-        greenBall(400);
-        blueBall(1200);
-        yellowBall(600);
-    }
-}
+// const RhythmConstructor = {
+//     nextStep: () => {
+//         // call each step at a time interval
+//     },
+//     rhythm: () => {
+//         redBall(1000);
+//         greenBall(400);
+//         blueBall(1200);
+//         yellowBall(600);
+//     },
+//     rhythm2: () => {
+//         redBall(1000);
+//         greenBall(400);
+//         blueBall(1200);
+//         yellowBall(600);
+//     }
+// }
 
-RhythmConstructor.rhythm1();
+// RhythmConstructor.rhythm1();
+
